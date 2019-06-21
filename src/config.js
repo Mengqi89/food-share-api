7d580176a8d12c21c2163c8592b0ce3c976d7d75
Mengqi Wang <wmq516@gmail.com>
2019-06-21 15:13:17 -0600

    hooked users-router with database

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  CLIENT_ORIGIN: 'https://food-share-client.wmq516.now.sh'
+  CLIENT_ORIGIN: 'https://food-share-client.wmq516.now.sh',
+  DB_URL: process.env.DB_URL || 'postgresql://Mengqi89@localhost/food-share'
 }
--- src/users/users-router.js
+++ src/users/users-router.js
@@ -1,21 +1,24 @@
 const express = require('express')
+const path = require('path')
+const UsersService = require('./users-service')
 
 const usersRouter = express.Router()
-const users = require('../../users')
 const jsonBodyParser = express.json()
 
 usersRouter
     .route('/')
-    .get((req, res) =>
-        res
-            .status(200)
-            .send(users)
-    )
-    .post(jsonBodyParser, (req, res) => {
-        const { id, first_name, last_name, email, username, password } = req.body
-        const newUser = { id, first_name, last_name, email, username, password }
+    .get((req, res, next) => {
+        UsersService.getAllUsers(req.app.get('db'))
+            .then(users => {
+                res.json(users)
+            })
+            .catch(next)
+    })
+    .post(jsonBodyParser, (req, res, next) => {
+        const { first_name, last_name, email, username, password } = req.body
+        const tempUser = { first_name, last_name, email, username, password }
 
-        for (const [key, value] of Object.entries(newUser))
+        for (const [key, value] of Object.entries(tempUser))
             if (value == null)
                 return res
                     .status(400)
@@ -23,24 +26,48 @@ usersRouter
                         error: { message: `Missing '${key}' in request body` }
                     })
 
-        const matchedUser = users.filter(user => user.username === username)
-        const matchedEmail = users.filter(user => user.email === email)
-
-        if (matchedUser.length !== 0) {
+        const passwordError = UsersService.validatePassword(password)
+        if (passwordError) {
             return res
-                .status(409)
-                .json({ error: { message: 'username already taken' } })
+                .status(400)
+                .json({ error: passwordError })
         }
-        if (matchedEmail.length !== 0) {
-            return res
-                .status(409)
-                .json({ error: { message: 'email already taken' } })
-        }
-        users.push(newUser)
-        res
-            .status(201)
-            .json({ message: 'Registration successful.' })
+        UsersService.hasUserWithEmail(req.app.get('db'), username)
+            .then(hasUserWithEmail => {
+                if (hasUserWithEmail)
+                    return res
+                        .status(400)
+                        .json({ error: { message: 'email already taken' } })
+            })
+        UsersService.hasUserWithUserName(req.app.get('db'), username)
+            .then(hasUserWithUserName => {
+                if (hasUserWithUserName)
+                    return res
+                        .status(400)
+                        .json({
+                            error: { message: 'username already taken' }
+                        })
+                return UsersService.hashPassword(password)
+                    .then(hashedPassword => {
+                        const newUser = {
+                            first_name,
+                            last_name,
+                            email,
+                            username,
+                            password: hashedPassword,
+                            date_created: 'now()'
+                        }
 
+                        return UsersService.insertUser(req.app.get('db'), newUser)
+                            .then(user => {
+                                res
+                                    .status(201)
+                                    .location(path.posix.join(req.originalUrl, `/${user.id}`))
+                                    .json(UsersService.serializeUser(user))
+                            })
+                    })
+            })
+            .catch(next)
     })
 
 module.exports = usersRouter
