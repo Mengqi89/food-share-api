d572d2ef5eb658f8aa074c9817f64d0bcf9c8a43
Mengqi Wang <wmq516@gmail.com>
2019-06-24 11:12:29 -0600

    initial attempt at deploying database

--- src/list/list-router.js
+++ src/list/list-router.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const path = require('path')
-const list = require('../../list')
 const jsonParser = express.json()
 const ListService = require('./list-service')
 const UsersService = require('../users/users-service')
